07448022eec5ce5c91ea2362e4f24b69f42c89b8
zepedroc <zepedrocm@hotmail.com>
2022-09-23 10:51:24 +0100

    Player Movement

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { FPV } from './components/FPV';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
 
 function App() {
   return (
+    <>
     <Canvas>
       <Sky sunPosition={[100, 100, 20]} />
       <ambientLight intensity={0.5} />
+      <FPV />
       <Physics>
         <Player />
         <Ground />
       </Physics>
     </Canvas>
+    <div className='absolute centered cursor'>+</div>
+    </>
   );
 }
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,8 +2,14 @@ import { useFrame, useThree } from "@react-three/fiber"
 import { useSphere } from "@react-three/cannon";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
+
+const JUMP_FORCE = 5;
+const SPEED = 5;
 
 export const Player = () => {
+    const actions = useKeyboard();
+
     const { camera } = useThree();
     const [ref, api] = useSphere(() => ({
         mass: 1,
@@ -27,6 +33,31 @@ export const Player = () => {
 
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
+
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (actions.moveBackward ? 1 : 0) - (actions.moveForward ? 1 : 0), // stop if we're pressing w and s at the same time
+        );
+        const sideVector = new Vector3(
+            (actions.moveLeft ? 1 : 0) - (actions.moveRight ? 1 : 0),
+            0,
+            0,
+        );
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation)
+
+        api.velocity.set(direction.x, vel.current[1], direction.z);
+  
+        if (actions.jump && Math.abs(vel.current[1]) < 0.05) {
+            api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+        }
     })
 
     return (
